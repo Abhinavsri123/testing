dc534de1b511f716fb562beb256275013e5b96c8
Abhinav <abhinavsrivastava54321@gmail.com>
2017-10-27 20:32:11 +0530

    heyy

---
+++ app/src/main/java/com/example/newu/pushup/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.newu.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        est();
+    }
+    private void est(){
+        Toast.makeText(this, "Maa Chuda", Toast.LENGTH_SHORT).show();
+    }
+}
